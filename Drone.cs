55b368f6c1e863f43b0e082c318fb4ff4809cfef
AaronSirico-Columbia <144375285+AaronSirico-Columbia@users.noreply.github.com>
2023-10-10 17:57:16 -0500

    Add files via upload

---
+++ Drone.cs
@@ -0,0 +1,25 @@
+﻿using OOPFlyingVehicleCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOPFlyingVehicle
+{
+    public class Drone : AerialVehicle, IHasBatteries
+    {
+        public Drone()
+        {
+            this.MaxAltitude = 500;
+        }
+
+        public bool HasBatteries { get; set; }
+        public bool IsCharged { get; set; }
+        public int ChargeLeft { get; set; }
+
+        public int ReturnCharge()
+        {
+            return ChargeLeft;
+        }
+    }
+}
---
+++ RemoteControlPlane.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPFlyingVehicleCore
+{
+    internal class RemoteControlPlane : IHasBatteries
+    {
+        public bool HasBatteries { get; set; }
+        public bool IsCharged { get; set; }
+        public int ChargeLeft { get; set; }
+
+        public int ReturnCharge()
+        {
+            return ChargeLeft;
+        }
+    }
+}
